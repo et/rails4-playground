04b28c3b3b63575e913727263a3df98aa36af043
Eric Thomas <eric.l.m.thomas@gmail.com>
2013-01-09 14:56:44 -0800

    initial generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4Playground::Application.config.secret_key_base = '5acf90e8e473152dabcb53098426dd29fbfa06978c6a7e3151bd1818b1f9a8f39b0dc1b5e4835d9128ad8ef4f67edaa39a4037fb5cce69c27e9adc6b14841815'
